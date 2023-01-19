726acc8adafd1a7dafa212481fcb115ec84452eb
Aleksey <96611699+Deusser@users.noreply.github.com>
2023-01-19 20:17:51 +0300

    test2

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -14,11 +14,16 @@ public class GameManager : MonoBehaviour
     public float Speed;
     public bool go;
     private Vector3 target;
+    [SerializeField] private Vector3[] Points = new Vector3[3];
+    public GameObject Cube;
+
+
 
     void Start()
     {
         Vector3[] Targets = new[] { point1.position, point2.position, point3.position, point4.position, point5.position }; 
         target = Targets[0];
+        
     }
 
 
